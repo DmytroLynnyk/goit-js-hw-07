d326b5e4923309d61f5a7fdd96a3af7c1b0012d2
DmytroLynnyk <138771794+DmytroLynnyk@users.noreply.github.com>
2023-11-17 17:48:16 +0200

    Task 02 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -6,14 +6,21 @@ const markup = galleryItems
     ({ preview, original, description }) =>
       `<li class="gallery__item">
             <a class="gallery__link" href="${original}">
-             <img
-             class="gallery__image"
-             src="${preview}"
-             data-source="${original}"
-             alt="${description}"
-             />
-             </a>
+                <img
+                class="gallery__image"
+                src="${preview}"
+                alt="${description}"/>
+            </a>
         </li>`
   )
   .join("");
 gallery.insertAdjacentHTML("beforeend", markup);
+
+gallery.addEventListener("click", (event) => {
+  event.preventDefault();
+});
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
